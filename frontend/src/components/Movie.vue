d6e2c66df47145b1ef7794e066a00e026622d13b
Margaux Renouf <margaux.renouf@student-cs.fr>
2022-06-03 17:34:17 +0200

    FrontEndV1

---
+++ frontend/src/components/Movie.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      {{ movie.original_title }}
+      {{ movie.released_date }}
+    </div>
+    <img :src="'https://image.tmdb.org/t/p/w200' + movie.poster_path" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Movie",
+  props: {
+    movie: Object,
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Welcome to Your Vue.js App</h1>
+    <input type="text" />
+    <div class="grid">
+      <Movie v-for="movie in movies" v-bind:key="movie.id" :movie="movie" />
+    </div>
+    <p>Research: {{ movieName }}</p>
+    <input v-model="movieName" placeholder="Film Name" />
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -77,15 +83,58 @@
 </template>
 
 <script>
+import Movie from "@/components/Movie.vue";
+import axios from "axios";
 export default {
   name: "Home",
+  components: {
+    Movie,
+  },
+  data: function () {
+    return {
+      movieName: "",
+      movies: [],
+    };
+  },
+  methods: {
+    fetchMovies: function () {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=522d421671cf75c2cba341597d86403a&language=en-US&page=1`
+        )
+        .then((response) => {
+          console.log(response);
+          this.movies = response.data.results;
+        })
+
+        .catch((error) => {
+          this.usersLoadingError = "An error occured while fetching users.";
+          console.error(error);
+        });
+    },
+  },
+  created: function () {
+    console.log(`the component is now created.`);
+    this.fetchMovies();
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.font-face {
+  font-family: Capuche;
+  src: url("C:\Users\Margaux\Downloads\capuche\Capuche Trial.otf");
+  font-weight: bold;
+}
+
 .home {
   text-align: center;
+}
+
+.grid {
+  display: flex;
+  flex-wrap: wrap;
 }
 
 h3 {
@@ -106,3 +155,4 @@ a {
   color: #42b983;
 }
 </style>
+}
